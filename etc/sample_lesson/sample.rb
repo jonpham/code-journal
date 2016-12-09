e4803ab7df3fa4e05829a1f27130bfd981148f6a
Jonathan Pham <jon.pham@gmail.com>
2016-12-08 17:09:18 -0800

    Add Some Etc Files for the datbase eer as well as some sample lesson template.

---
+++ etc/sample_lesson/sample.rb
@@ -0,0 +1,37 @@
+require 'rspec'
+require 'cj_code_runner'
+
+class SampleLesson
+include CodeRunner
+
+  def initialize
+  end
+  # User Code
+
+  def say_hello
+    puts "hello world!"
+  end
+
+  def say_words(word1,word2)
+    puts "This is me saying, #{word1} and #{word2}"
+  end
+
+  # Runner Code
+  def module1(data_hash)
+
+  end
+
+  def module2(data_hash)
+    
+  end
+
+end
+
+RSpec.describe SampleLesson do 
+  describe '#module1' do 
+    it 'should return the sum of all items in the array' do
+      uut = SampleLesson.new 
+      expect(uut.module1(test_set1)).to eq(15)
+    end
+  end
+end
