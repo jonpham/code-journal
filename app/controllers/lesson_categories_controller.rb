ce4bfc14f3f4beadd36ae9b2e08321ac8dcdeb6a
Jonathan Pham <jon.pham@gmail.com>
2016-12-11 15:53:28 -0800

    Add LessonCategory index/new/create/show controls/views.

--- app/controllers/lesson_categories_controller.rb
+++ app/controllers/lesson_categories_controller.rb
@@ -1,2 +1,25 @@
 class LessonCategoriesController < ApplicationController
+
+  def index 
+    @categories = LessonCategory.all
+  end
+
+  def new
+    @new_category = LessonCategory.new
+  end
+
+  def create
+    @new_category = LessonCategory.new({category_name: params[:input_category_name], 
+      category_description: params[:input_description]})
+    if @new_category.save
+      redirect_to '/lesson_categories/' # Redirect to lesson Creation Wizard after.
+    else
+      flash[:danger] = "Something went wrong with the Category Creation!"
+      render :new
+    end
+  end
+
+  def show 
+    @category = LessonCategory.find_by(id: params[:id])
+  end
 end
