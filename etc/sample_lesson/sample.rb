1dd1256b29ddeb4947d741c3f799b70dacb4fe70
Jonathan Pham <jon.pham@gmail.com>
2016-12-11 12:55:31 -0800

    Add More to the Sample Lessons.

--- etc/sample_lesson/sample.rb
+++ etc/sample_lesson/sample.rb
@@ -1,5 +1,5 @@
 require 'rspec'
-require 'cj_code_runner'
+# require 'cj_code_runner'
 
 # include CodeRunner
 class SampleLesson
@@ -8,28 +8,56 @@ class SampleLesson
   # User Code
 
   def say_hello
-    puts 'hello world!'
+    string = 'This is me saying, hello world!'
+    return string
   end
 
   def say_words(word1, word2)
-    puts "This is me saying, #{word1} and #{word2}"
+    string = "This is me saying, #{word1} #{word2}"
+    return string
+  end
+
+  def say_string_array(input_array)
+    string = "This is me saying, #{input_array[0]} #{input_array[1]}"
+    return string
   end
 
   # Runner Code
-  def module1
-    say_hello
+  def module1(_args)
+    return say_hello
   end
 
-  def module2(args)
-    say_words(args[0], args[1])
+  def module2(_args)
+    return say_words(_args[0], _args[1])
+  end
+
+  def module3(_args)
+    return say_string_array(_args)
   end
 end
 
 RSpec.describe SampleLesson do 
   describe '#module1' do 
-    it 'should return the sum of all items in the array' do
+    it 'should return "This is me saying, hello world!"' do
       uut = SampleLesson.new 
-      expect(uut.module1(test_set1)).to eq(15)
+      test_set1=[]
+      expect(uut.module1(test_set1)).to eq('This is me saying, hello world!')
+    end
+  end
+
+  describe '#module2' do 
+    it 'should return "This is me saying, hello world!"' do
+      uut = SampleLesson.new 
+      test_set2=["hello","world!"]
+      expect(uut.module2(test_set2)).to eq('This is me saying, hello world!')
+    end
+  end
+
+  describe '#module3' do 
+    it 'should return "This is me saying, hello world!"' do
+      uut = SampleLesson.new 
+      test_set3=["hello","world!"]
+      expect(uut.module3(test_set3)).to eq('This is me saying, hello world!')
     end
   end
 end
--- etc/sample_lesson/sample_lesson.md
+++ etc/sample_lesson/sample_lesson.md
@@ -1,18 +1,33 @@
-#h1 This page shall Serve as the Example Lesson Description Content.
+#This page shall Serve as the Example Lesson Description Content.
 
-**Sample Lesson**
+##**Sample Lesson**
 
-**Concept Description**
+###**Concept Description**
 
-**Relevancy / Why It's Important**
+###**Relevancy / Why It's Important**
 
-**Example Exercise**
-**Problem Context**
-**CodeExecution**
-**AcceptanceCriteria**
+###**Example Exercise**
+###**Problem Context**
+###**CodeExecution**
+###**AcceptanceCriteria**
 
-**Module1**
+####**Module1**
+```
+def say_hello
+    # Return Appropriate String
+end
+```
 
-**Module2**
+####**Module2**
+```
+def say_words(word1, word2)
+    # Return Appropriate String
+end
+```
 
-**Module3**
+####**Module3**
+```
+def say_worlds(input_array=[])
+    # Return Appropriate String
+end
+```
