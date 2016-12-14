8425a4fe83914b6ca36da4124a101404069990e0
Jonathan Pham <jon.pham@gmail.com>
2016-12-13 21:01:29 -0800

    Add Base Rails Controller for Testcode.

---
+++ app/controllers/test_codes_controller.rb
@@ -0,0 +1,2 @@
+class TestCodesController < ApplicationController
+end
---
+++ test/controllers/test_codes_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TestCodesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
