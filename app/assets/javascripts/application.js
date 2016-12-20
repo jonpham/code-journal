02f1c1857007a185ccece028d279e76f1d08166c
Jonathan Pham <jon.pham@gmail.com>
2016-12-20 13:46:10 -0800

    Found that RedCarpet / Rouge is only partially working.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,6 @@
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require ace/ace
-//= require simplemde
+// require simplemde
 //= require turbolinks
 //= require_tree .
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,29 +4,32 @@ module ApplicationHelper
   require 'rouge'
   require 'rouge/plugins/redcarpet'
 
-  class HTML < Redcarpet::Render::HTML
+  class HTMLrc < Redcarpet::Render::HTML
     include Rouge::Plugins::Redcarpet
 
-    def block_code(code, language)
-      Rouge.highlight(code, language || 'text', 'html')
-    end
+    # def block_code(code, language,to='html')
+    #   return Rouge.highlight(code, language || 'text', 'ruby');
+    # end
   end
 
-  def markdown(text)
+  def to_markdown(text)
     render_options = {
       filter_html:     true,
-      hard_wrap:       true, 
-      link_attributes: { rel: 'nofollow' }
+      hard_wrap:       true,
+      link_attributes: { rel: 'nofollow', target: "_blank" },
+      space_after_headers: true,
+      fenced_code_blocks: true
     }
-    renderer = HTML.new(render_options)
 
+    renderer = HTMLrc.new(render_options)
+    # raise Markdown
     extensions = {
       autolink:           true,
-      fenced_code_blocks: true,
       lax_spacing:        true,
       no_intra_emphasis:  true,
       strikethrough:      true,
-      superscript:        true
+      superscript:        true,
+      disable_indented_code_blocks: false
     }
     return Redcarpet::Markdown.new(renderer, extensions).render(text).html_safe
   end
