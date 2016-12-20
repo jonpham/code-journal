c6c290919d4a804b764e28dfb15ee59adbe6a61d
Jonathan Pham <jon.pham@gmail.com>
2016-12-19 18:42:26 -0800

    Got Rouge/RedCarpet to Work.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,33 @@
 module ApplicationHelper
+
+  require 'redcarpet'
+  require 'rouge'
+  require 'rouge/plugins/redcarpet'
+
+  class HTML < Redcarpet::Render::HTML
+    include Rouge::Plugins::Redcarpet
+
+    def block_code(code, language)
+      Rouge.highlight(code, language || 'text', 'html')
+    end
+  end
+
+  def markdown(text)
+    render_options = {
+      filter_html:     true,
+      hard_wrap:       true, 
+      link_attributes: { rel: 'nofollow' }
+    }
+    renderer = HTML.new(render_options)
+
+    extensions = {
+      autolink:           true,
+      fenced_code_blocks: true,
+      lax_spacing:        true,
+      no_intra_emphasis:  true,
+      strikethrough:      true,
+      superscript:        true
+    }
+    return Redcarpet::Markdown.new(renderer, extensions).render(text).html_safe
+  end
 end
