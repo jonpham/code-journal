3711b443be51a9677d664a2a251481d813aa2ac4
Jonathan Pham <jon.pham@gmail.com>
2017-01-15 00:26:55 -0800

    Return 'yAML' require and remove unneccessary file.

--- lib/tasks/tests/load_commands.rb
+++
@@ -1,4 +0,0 @@
-load 'TestData.rb'
-lesson_data = Testing::TestDataHandler.read_yaml_file('./data/class_code_builder.yml')
-lesson_data[:class_methods]["initialize"][:builder].arguments
-lesson_data[:class_methods]["initialize"][:builder].build_code
